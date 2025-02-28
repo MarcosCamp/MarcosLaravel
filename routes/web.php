9ce57c33bafd28145700000eb3846af5e486e0a0
Marcos <marcoscampserrano@gmail.com>
2025-02-28 13:32:40 +0100

    establecemos la ruta personajes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PersonajeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,4 +18,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::resource('personajes', PersonajeController::class);
 require __DIR__.'/auth.php';
