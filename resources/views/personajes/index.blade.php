e7fa023545966a2637ee867bdc2b5d79e61ab527
Marcos <marcoscampserrano@gmail.com>
2025-02-28 14:01:29 +0100

    index con componentes

--- resources/views/personajes/index.blade.php
+++ resources/views/personajes/index.blade.php
@@ -1,37 +1,41 @@
-@extends('layouts.guest')
+<!-- resources/views/components/personajes-table.blade.php -->
 
-@section('content')
-    <h1>Personajes</h1>
-    <a href="{{ route('personajes.create') }}">Crear nuevo personaje</a>
-
+<table class="min-w-full table-auto border-collapse bg-white shadow-md rounded-lg">
+    <thead>
+    <tr>
+        <th class="py-2 px-4 border-b text-left">Nombre</th>
+        <th class="py-2 px-4 border-b text-left">Tipo</th>
+        <th class="py-2 px-4 border-b text-left">Categoría</th>
+        <th class="py-2 px-4 border-b text-left">Vida</th>
+        <th class="py-2 px-4 border-b text-left">Hipercarga</th>
+        <th class="py-2 px-4 border-b text-left">Skins</th>
+        <th class="py-2 px-4 border-b text-left">Acciones</th>
+    </tr>
+    </thead>
+    <tbody>
     @foreach($personajes as $personaje)
-        @if($personaje)  <!-- Aseguramos que $personaje no sea null -->
-        <div>
-            <h3>{{ $personaje->nombre }} ({{ $personaje->tipo }})</h3>
-            <p>Categoria: {{ $personaje->categoria }}</p>
-            <p>Vida: {{ $personaje->vida }}</p>
-            <p>Hipercarga: {{ $personaje->hipercarga ? 'Sí' : 'No' }}</p>
-
-            <h4>Skins:</h4>
-            @if($personaje->skins->isNotEmpty())  <!-- Verifica si tiene skins -->
-            <ul>
-                @foreach($personaje->skins as $skin)
-                    <li>{{ $skin->nombre }}</li>
-                @endforeach
-            </ul>
-            @else
-                <p>No tiene skins.</p>
-            @endif
-
-            <a href="{{ route('personajes.edit', $personaje->id) }}">Editar</a>
-            <form action="{{ route('personajes.destroy', $personaje->id) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit">Eliminar</button>
-            </form>
-        </div>
-        @else
-            <p>Este personaje no existe.</p>  <!-- Si el personaje es null, se muestra un mensaje -->
-        @endif
+        <tr>
+            <td class="py-2 px-4 border-b">{{ $personaje->nombre }}</td>
+            <td class="py-2 px-4 border-b">{{ $personaje->tipo }}</td>
+            <td class="py-2 px-4 border-b">{{ $personaje->categoria }}</td>
+            <td class="py-2 px-4 border-b">{{ $personaje->vida }}</td>
+            <td class="py-2 px-4 border-b">{{ $personaje->hipercarga ? 'Sí' : 'No' }}</td>
+            <td class="py-2 px-4 border-b">
+                <ul>
+                    @foreach($personaje->skins as $skin)
+                        <li>{{ $skin->nombre }}</li>
+                    @endforeach
+                </ul>
+            </td>
+            <td class="py-2 px-4 border-b">
+                <a href="{{ route('personajes.edit', $personaje->id) }}" class="text-blue-500 hover:text-blue-700">Editar</a>
+                <form action="{{ route('personajes.destroy', $personaje->id) }}" method="POST" class="inline-block ml-2">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="text-red-500 hover:text-red-700">Eliminar</button>
+                </form>
+            </td>
+        </tr>
     @endforeach
-@endsection
+    </tbody>
+</table>
